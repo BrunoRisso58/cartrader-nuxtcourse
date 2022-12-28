b34be4c59c77b583536db9b82b2fdf7e7095c80d
Bruno Risso <brunorisso58@gmail.com>
2022-12-28 19:49:26 -0300

    Defining layouts

---
+++ pages/car/[name]-[id].vue
@@ -0,0 +1,19 @@
+<script setup>
+const route = useRoute();
+useHead({
+    title: route.params.name
+})
+
+definePageMeta({
+    layout: "custom",
+});
+</script>
+
+<template>
+    <div>
+        <CarDetailHero />
+        <CarDetailAttributes />
+        <CarDetailDescription />
+        <CarDetailContact />
+    </div>
+</template>
---
+++ pages/city/[city]/car.vue
@@ -0,0 +1,20 @@
+<script setup>
+const route = useRoute();
+useHead({
+    title: `${route.params.make ? route.params.make : "Cars"} in ${route.params.city}`,
+});
+
+definePageMeta({
+    layout: "custom"
+})
+
+</script>
+
+<template>
+    <div>
+        <div class="mt-32 flex">
+            <CarSideBar />
+            <NuxtPage />
+        </div>
+    </div>
+</template>
